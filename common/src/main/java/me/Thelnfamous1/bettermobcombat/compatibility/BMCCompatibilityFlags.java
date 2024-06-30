e83569aa085b029eaadd875273b181c0cad5bd95
Thelnfamous1 <junior.garay1999@gmail.com>
2024-06-30 01:26:44 -0500

    Fixed Pehkui compatibility

--- common/src/main/java/me/Thelnfamous1/bettermobcombat/compatibility/BMCCompatibilityFlags.java
+++ common/src/main/java/me/Thelnfamous1/bettermobcombat/compatibility/BMCCompatibilityFlags.java
@@ -1,13 +1,12 @@
 package me.Thelnfamous1.bettermobcombat.compatibility;
 
 import net.bettercombat.Platform;
-import net.bettercombat.compatibility.PehkuiHelper;
 
 public class BMCCompatibilityFlags {
 
     public static void initialize() {
         if (Platform.isModLoaded("pehkui")) {
-            PehkuiHelper.load();
+            BMCPehkuiHelper.load();
         }
     }
 }
